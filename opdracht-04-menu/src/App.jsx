ba7acf679e000e091882f158a29bf39741a45501
Sulaimanb1012 <9024289@student.tcrmbo.nl>
2025-09-23 19:57:24 +0200

    Opdracht 4 Menu

--- opdracht-04-menu/src/App.jsx
+++ opdracht-04-menu/src/App.jsx
@@ -1,12 +1,30 @@
-import './App.css'
+import { useState } from "react";
+import "./App.css";
+import menuData from "./data.js";
 
 function App() {
+  const [items] = useState(menuData);
 
   return (
     <>
       <h1>Opdracht 4 - Menu</h1>
+
+      <section className="section-center">
+        {items.map(({ id, title, price, img, desc }) => (
+          <article key={id} className="menu-item">
+            <img src={img} alt={title} className="photo" />
+            <div className="item-info">
+              <header>
+                <h4>{title}</h4>
+                <h4 className="price">{price}</h4>
+              </header>
+              <p className="item-text">{desc}</p>
+            </div>
+          </article>
+        ))}
+      </section>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
