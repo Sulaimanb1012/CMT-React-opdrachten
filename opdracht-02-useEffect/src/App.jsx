0d2f3130925f62789acb7f54a9041d25d8af64c7
Sulaimanb1012 <9024289@student.tcrmbo.nl>
2025-09-23 11:51:34 +0200

    Opdracht 2

--- opdracht-02-useEffect/src/App.jsx
+++ opdracht-02-useEffect/src/App.jsx
@@ -1,6 +1,6 @@
 import { useState, useEffect } from "react";
 import './App.css';
-import WelcomeMessage from './WelcomeMessage';
+import WelcomeMessage from './components/WelcomeMessage';
 
 function App() {
   const [currentTime, setCurrentTime] = useState(new Date());
