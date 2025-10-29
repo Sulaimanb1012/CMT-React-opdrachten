b92857e7ae7b121a2eed9ceb74c2824003e60951
Sulaimanb1012 <9024289@student.tcrmbo.nl>
2025-10-29 21:59:41 +0100

    opdracht 11

--- opdracht-01-books/src/App.jsx
+++ opdracht-01-books/src/App.jsx
@@ -1,15 +1,25 @@
+import { useState } from "react";
 import Header from "./components/Header";
 import Layout from "./components/Layout";
 import BookList from "./components/BookList";
+import SearchBar from "./components/SearchBar";
 import "./App.css";
 
 function App() {
+  const [search, setSearch] = useState("");
+
+
+  const handleSearch = (value) => {
+    setSearch(value);
+  };
+
   return (
     <>
       <Header />
       <Layout>
         <h1>Amazon Best Sellers</h1>
-        <BookList />
+        <SearchBar onSearch={handleSearch} />
+        <BookList search={search} />
       </Layout>
     </>
   );
--- opdracht-01-books/src/components/Booklist.jsx
+++ opdracht-01-books/src/components/Booklist.jsx
@@ -1,37 +1,28 @@
-import React, { useState } from "react";
+import { useState } from "react";
 import Book from "./Book";
 import BookCounter from "./BookCounter";
 
-function BookList() {
+function BookList({ search }) {
   const [books] = useState([
-    {
-      title: "Harry Ptter",
-      author: "J.K Rowling",
-      image: "/images/book-1.png",
-    },
-    {
-      title: "Fantasia VI",
-      author: "Geronimo Stilton",
-      image: "/images/book-2.png",
-    },
-    {
-      title: "The Hunger Games",
-      author: "Suzanna Collins",
-      image: "/images/book-3.png",
-    },
+    { title: "Harry Potter", author: "J.K. Rowling", image: "/images/book-1.png" },
+    { title: "Fantasia VI", author: "Geronimo Stilton", image: "/images/book-2.png" },
+    { title: "The Hunger Games", author: "Suzanne Collins", image: "/images/book-3.png" },
   ]);
+
+  const filteredBooks = books.filter((book) =>
+    book.title.toLowerCase().includes(search.toLowerCase())
+  );
 
   return (
     <div className="booklist">
-      <BookCounter aantal={books.length} />
-      {books.map((book, index) => (
-        <Book
-          key={index}
-          title={book.title}
-          author={book.author}
-          image={book.image}
-        />
-      ))}
+      <BookCounter aantal={filteredBooks.length} />
+      {filteredBooks.length > 0 ? (
+        filteredBooks.map((book, index) => (
+          <Book key={index} title={book.title} author={book.author} image={book.image} />
+        ))
+      ) : (
+        <p>Geen boeken gevonden.</p>
+      )}
     </div>
   );
 }
